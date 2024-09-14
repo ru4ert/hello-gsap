d9a2e931a8bb64d2c453576404000d2da693746f
rupertbogensperger <rupert.bogensperger@admiral.at>
2024-09-14 08:18:52 +0200

    imp useGSAP

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client"
 import RotatingIcons from "@/components/RotatingIcons";
+import {useRef} from "react";
 
 export default function Home() {
+    const containerRef = useRef<HTMLDivElement>(null);
     return (
-        <div className="flex flex-col">
-            <RotatingIcons/>
+        <div className="flex flex-col" ref={containerRef}>
+            <RotatingIcons containerRef={containerRef}/>
         </div>
     );
 }
--- components/RotatingIcons/index.tsx
+++ components/RotatingIcons/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useEffect, useRef} from 'react';
+import React, {useRef} from 'react';
 import {gsap} from 'gsap';
 import {MotionPathPlugin} from 'gsap/MotionPathPlugin';
 import {MotionPathHelper} from 'gsap-trial/MotionPathHelper';
@@ -7,6 +7,7 @@ import {CustomEase} from 'gsap/CustomEase';
 import {Icon} from "@/components/Icon";
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import Image from "next/image";
+import {useGSAP} from "@gsap/react";
 
 gsap.registerPlugin(
     MotionPathHelper,
@@ -14,27 +15,34 @@ gsap.registerPlugin(
     ScrollTrigger
 );
 const circularPath = "M-306.92,6.323 C-306.92,-55.035 -113.133,-116.538 24.866,-116.538 153.841,-116.538 336.37,-59.208 340.197,23.694 343.745,101.341 231.546,162.159 20.577,162.159 -171.334,162.159 -301.881,98.177 -301.893,17.512"
-const RotatingIcons: React.FC = () => {
+type RotatingIconsProps = {
+    containerRef: React.RefObject<HTMLDivElement>;
+}
+const RotatingIcons: React.FC<RotatingIconsProps>= ((props) => {
     const iconRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        if (!iconRef.current) return;
-        const icons = iconRef.current.querySelectorAll('img');
-        const duration = 2;
-        icons.forEach((icon, index) => {
-            gsap!.to(icon, {
-                duration,
-                repeat: -1,
-                ease: CustomEase.create("custom", "M0,0 C0,0 0.09,0.09 0.216,0.216 0.312,0.312 0.355,0.464 0.47,0.579 0.564,0.673 0.731,0.731 0.808,0.808 0.921,0.921 1,1 1,1 "),
-                motionPath: {
-                    path: circularPath,
-                },
-                onStart: function () {
-                    this.progress((index / icons.length) || 1);
-                },
+    useGSAP(
+        () => {
+            if (!iconRef.current) return;
+            const icons = iconRef.current.querySelectorAll('img');
+            const duration = 2;
+            icons.forEach((icon, index) => {
+                gsap!.to(icon, {
+                    duration,
+                    repeat: -1,
+                    ease: CustomEase.create("custom", "M0,0 C0,0 0.09,0.09 0.216,0.216 0.312,0.312 0.355,0.464 0.47,0.579 0.564,0.673 0.731,0.731 0.808,0.808 0.921,0.921 1,1 1,1 "),
+                    motionPath: {
+                        path: circularPath,
+                    },
+                    onStart: function () {
+                        this.progress((index / icons.length) || 1);
+                    },
+                });
             });
-        });
-    }, []);
+        },
+        {dependencies: [], scope: props.containerRef}
+    );
+
     return (
         <>
             <div className="flex items-center justify-center min-h-screen bg-gray-900">
@@ -69,6 +77,8 @@ const RotatingIcons: React.FC = () => {
             </div>
         </>
     )
-};
+});
+
+RotatingIcons.displayName = 'RotatingIcons';
 
 export default RotatingIcons;
